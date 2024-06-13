b6677ab04f29bed8c4c5de9ec32a987f80c8b2a6
Nahuel Godoy <nahuelg.1992@gmail.com>
2024-06-13 15:55:43 -0300

    Comenzando facturación

--- src/app/pantallas/pantalla-administracion/articulos/consultar-lista-precios/consultar-lista-precios.component.ts
+++ src/app/pantallas/pantalla-administracion/articulos/consultar-lista-precios/consultar-lista-precios.component.ts
@@ -2,18 +2,44 @@ import { Component } from '@angular/core';
 import { EncabezadoComponent } from '../../../../components/encabezado/encabezado.component';
 import { RespuestaServerComponent } from '../../../../components/respuesta-server/respuesta-server.component';
 import { BotonCargarComponent } from '../../../../components/boton-cargar/boton-cargar.component';
+import { CommonModule } from '@angular/common';
+import { PeticionesHttpService } from '../../../../services/peticiones-http.service';
 
 @Component({
-  selector: 'app-consultar-lista-precios',
-  standalone: true,
-  imports: [
-    EncabezadoComponent,
-    RespuestaServerComponent,
-    BotonCargarComponent
-  ],
-  templateUrl: './consultar-lista-precios.component.html',
-  styleUrl: './consultar-lista-precios.component.scss'
+    selector: 'app-consultar-lista-precios',
+    standalone: true,
+    templateUrl: './consultar-lista-precios.component.html',
+    styleUrl: './consultar-lista-precios.component.scss',
+    imports: [
+        EncabezadoComponent,
+        RespuestaServerComponent,
+        CommonModule,
+        BotonCargarComponent
+    ]
 })
 export class ConsultarListaPreciosComponent {
+  listaArticulos : any[] = [];
 
+  constructor(
+    private _peticionesHttp : PeticionesHttpService,
+  ) {
+    this.obtenerDatosListaPrecios();
+  }
+
+  obtenerDatosListaPrecios() {
+    this._peticionesHttp.listaArticulosConPrecios(0).subscribe({
+      next: (data) => {
+        if (data.error) {
+          this._peticionesHttp.setRespuestaServer(data.message);
+        } else {
+          this.listaArticulos = data.data;
+        }
+
+        console.log(this.listaArticulos);
+      },
+      error: (error) => {
+        this._peticionesHttp.setRespuestaServer(error.message);
+      }
+    });
+  }
 }
--- src/app/pantallas/pantalla-administracion/facturas/cargar-factura/cargar-factura.component.ts
+++ src/app/pantallas/pantalla-administracion/facturas/cargar-factura/cargar-factura.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { EncabezadoComponent } from '../../../../components/encabezado/encabezado.component';
+import { RespuestaServerComponent } from '../../../../components/respuesta-server/respuesta-server.component';
+import { PeticionesHttpService } from '../../../../services/peticiones-http.service';
 
 @Component({
   selector: 'app-cargar-factura',
   standalone: true,
-  imports: [],
+  imports: [
+    EncabezadoComponent,
+    RespuestaServerComponent
+  ],
   templateUrl: './cargar-factura.component.html',
   styleUrl: './cargar-factura.component.scss'
 })
 export class CargarFacturaComponent {
+  constructor(
+    private _peticionesHttp : PeticionesHttpService
+  ) {
 
+  }
 }
